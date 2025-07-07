08da90d98eb6b48049d294018dda0ec4dbfbaff6
aln <aln@lumpy.lan>
2025-07-07 03:30:28 +0100

    d

--- static.py
+++ static.py
@@ -90,7 +90,7 @@ all_posts = []
 for mdfile in Path('posts').glob('*.md'):
     with open(mdfile) as f:
         front = frontmatter.load(mdfile).to_dict()
-        fn = f"{mdfile.stem}.html"
+        fn = f"{front.get('url', mdfile.stem)}.html"
         front.update({'filename': fn})
         all_posts.append(front)
 
